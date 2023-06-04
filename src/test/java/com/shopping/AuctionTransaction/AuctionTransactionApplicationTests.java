07f1717b1f94e4df88f296fa803eb510b19c636b
jsw010115 <jsw0101151@naver.com>
2023-06-04 17:39:48 +0900

    git Test

---
+++ src/test/java/com/shopping/AuctionTransaction/AuctionTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopping.AuctionTransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
